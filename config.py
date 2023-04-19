8559feddd8223dd3abd17e79818d492a1706943a
Pratikllp <118655982+Pratikllp@users.noreply.github.com>
2023-04-19 10:21:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = "6189486646:AAGTN0wYQpYM3JXMRxuweXM6JNJzob96TBY"
+    BOT_TOKEN = "6229046626:AAGoLBjiGsOEeSdm8_GNDxrvwJnC2OD79k0"
     API_ID = 29410389
     API_HASH = "0c716764715886f6641477ffbb63e1ee"
     AUTH_USERS = '6253923773'
